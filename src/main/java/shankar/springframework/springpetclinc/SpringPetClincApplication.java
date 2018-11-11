5705f499dfb3ffb922f094dff59af8572788fe68
shankartech <shankar.javadev@gmail.com>
2018-11-11 11:17:42 +0530

    Initial Commit

---
+++ src/main/java/shankar/springframework/springpetclinc/SpringPetClincApplication.java
@@ -0,0 +1,12 @@
+package shankar.springframework.springpetclinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClincApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClincApplication.class, args);
+    }
+}
